6c78c622ef58e09b10919e167635a6d9ff8eb7ea
karthikjohnn <karthickyamkk2001@gmail.com>
2024-08-27 23:03:46 +0530

    Api integration

---
+++ hooks/GetDetails.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const getDetails = (url, options = {}) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(url, options);
+        setData(response.data);
+        console.log("hook");
+        
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default getDetails;
