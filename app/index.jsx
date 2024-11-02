b4a83e847cb505a0c7836aac03bde6b82bb856fd
karthikjohnn <karthickyamkk2001@gmail.com>
2024-11-02 11:19:37 +0530

    welcome screen animation added

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,8 @@ import { StatusBar } from "expo-status-bar";
 import { router } from "expo-router";
 import Loader from "../components/Loader";
 import Button from "../components/Button";
-
+import { FadeIn, FadeInDown } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
 export default function SplashScreen() {
   const [loader, setLoader] = useState(false);
   // useEffect(() => {
@@ -31,19 +32,24 @@ export default function SplashScreen() {
           className="object-contain w-full h-3/4 max-h-[550px] absolute top-0 left-0"
         />
         <View className="mt-auto  px-5">
-          <Text className="text-4xl text-white text-center font-Sora-SemiBold">
-            Fall in Love with Coffee in Blissful Delight!
-          </Text>
-          <Text className="text-base text-offgray text-center font-Sora-Regular mt-2">
-            Welcome to our cozy coffee corner, where every cup is a delightful
-            for you.
-          </Text>
-         
-          <Button
-            title={loader ? <Loader /> : "get started"}
-            style={"mt-6"}
-            onPress={handleOnClick}
-          />
+          <Animated.View entering={FadeInDown.delay(300).duration(300)}>
+            <Text className="text-4xl text-white text-center font-Sora-SemiBold">
+              Fall in Love with Coffee in Blissful Delight!
+            </Text>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(350).duration(300)}>
+            <Text className="text-base text-offgray text-center font-Sora-Regular mt-2">
+              Welcome to our cozy coffee corner, where every cup is a delightful
+              for you.
+            </Text>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(400).duration(300)}>
+            <Button
+              title={loader ? <Loader /> : "get started"}
+              style={"mt-6"}
+              onPress={handleOnClick}
+            />
+          </Animated.View>
         </View>
       </View>
       <StatusBar style="light" translucent backgroundColor="transparent" />
